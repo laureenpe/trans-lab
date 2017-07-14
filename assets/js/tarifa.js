4660ae55d9428ba0ac2b76a82bd08cf1b1e318dd
“Laura <laureen.pelaez.isaza@gmail.com>
2017-07-14 09:33:15 -0400

    tarifas js working and commented

--- assets/js/tarifa.js
+++ assets/js/tarifa.js
@@ -2,7 +2,10 @@ $(document).ready(function () {
     $('#calculate').click(taxes);//listener para el botón
     selectBip();
 });
+
+// ────────────────────────────────────────── I ─────────────
 //Llamado Ajax para el saldo de la tarjeta
+// ──────────────────────────────────────────────────────────
 function taxes() {
     var card_number = $("#card-number-calculate").val();
     var url = "http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip=" + card_number;
@@ -13,17 +16,15 @@ function taxes() {
         console.log(response);
         var schedule_select = parseFloat($('#schedule-select').val()); //Convertir a número entero 
         var balanceCard = parseFloat(response.saldoTarjeta.substring(1).replace('.', '')); //por defecto el número que retorna esta en formato $2.640 y esto interfiere con el cálculo
-        console.log("Saldo de la tarjeta" + + balanceCard);
+        console.log("Saldo de la tarjeta" + balanceCard);
         console.log("selector" + schedule_select);
 
         if (schedule_select != null) {
-            var totalAmount = balanceCard - schedule_select;
+            var totalAmount = balanceCard - schedule_select; //Calculo para conocer el saldo final
             $("#saldo-final").html(totalAmount);
             $("#total-costo").html(schedule_select);
-            console.log(totalAmount);
-            $(".hide-box").removeClass("hide");
+            $(".hide-box").removeClass("hide"); //Mostrar la caja con los datos del saldo
         }
-
 
     }).fail(function () {
         alert("Tarjeta Bip no válida");
@@ -31,25 +32,29 @@ function taxes() {
 
 }
 
-//Esta función busca poner el valor seleccionado de la tarjeta para poderlo utilizar en el llamado ajax
+// ────────────────────────────────────────── I ───────────────────────────────────────────────────────────────────────────────────────────────────────────────
+//  Esta función busca poner el valor seleccionado de la tarjeta en el input nùmero de tarjeta, para poderlo utilizar en el llamado ajax y me de un valor
+// ────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+
 function onChangeSelector() {
     var select_storage = $('#select-storage').val();
-    console.log(select_storage);
-
     $("#card-number-calculate").val(select_storage);//Asignar el valor del select-storage
 }
+
+
+// ────────────────────────────────────────── I ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+// Esta función busca en el local storage del js profile y saldo donde se guardo este item previamente, los datos guardados en un array.  Para poderlo usar como array se corto en comas, se utiliza el split
+// ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 
 function selectBip() {
     var cards = localStorage.getItem('cards').split(',');
     var html = '<select id="select-storage" ><option value="" disabled selected>Choose your option</option>';
-    for (var i = 0; i < cards.length; i++) {
-        html += '<option value="' + cards[i] + '">' + cards[i] + '</option>' ;
+    for (var i = 0; i < cards.length; i++) { //Recorrido del array cards para poderlo adicionar  al html
+        html += '<option value="' + cards[i] + '">' + cards[i] + '</option>';
     }
     html += '</select>';
-
     $('.container-saldo').html(html);
     $('#select-storage').change(onChangeSelector);
 }
 
 
-//        ONCHANGE
